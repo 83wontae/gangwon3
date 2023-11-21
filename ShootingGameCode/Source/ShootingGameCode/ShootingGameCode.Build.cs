1c959c724530a46a52b214e0d7267b404e2d18af
sky <hym990407@gmail.com>
2023-11-21 17:20:05 +0900

    312

--- ShootingGameCode/Source/ShootingGameCode/ShootingGameCode.Build.cs
+++ ShootingGameCode/Source/ShootingGameCode/ShootingGameCode.Build.cs
@@ -8,8 +8,10 @@ public class ShootingGameCode : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "UMG", "ItemPluginCode" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "UMG", "ItemPluginCode", "OnlineSubsystem", "OnlineSubsystemUtils" });
 
 		PublicIncludePaths.AddRange(new string[] { "ShootingGameCode", "ShootingGameCode/Public" });
-	}
+
+        DynamicallyLoadedModuleNames.Add("OnlineSubsystemNull");
+    }
 }
